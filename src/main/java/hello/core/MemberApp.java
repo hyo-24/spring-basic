b49785372990731c0a1f6903a8526d39e85d0bb8
Elena Lee <tbfjs3976@icloud.com>
2026-03-03 15:31:17 +0900

    3/2

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MemberApp {
+
+public class MemberApp { // 자바 코드로 테스트하는 클래스
 
     static void main() {
-        AppConfig appConfig = new AppConfig(); // main 에 관리자 객체 생성하기
-        MemberService memberService = appConfig.memberService();
+        // AppConfig appConfig = new AppConfig(); // main 에 관리자 객체 생성하기
+        // MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+
+
         Member member = new Member(1L, Grade.VIP, "memberA");
         memberService.join(member);
 
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,13 +7,19 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     static void main() {
-        AppConfig appConfig = new AppConfig(); // 관리자 객체로 아래 객체들을 생성해 주입
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        // AppConfig appConfig = new AppConfig(); // 관리자 객체로 아래 객체들을 생성해 주입
+        // MemberService memberService = appConfig.memberService();
+        // OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, Grade.VIP, "memberA");
