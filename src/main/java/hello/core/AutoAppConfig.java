d9876e173b45431f80d0d19108ea0839be771f7e
Elena Lee <tbfjs3976@icloud.com>
2026-03-12 16:47:42 +0900

    3/12

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.FilterType;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 
-@Configuration
-@ComponentScan(
+@Configuration // 설정파일 이다.
+@ComponentScan( // @SpringBootApplication 에 들어있는 애노테이션이라 직접 사용하지 않았었음.
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 
 public class AutoAppConfig {
---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,38 @@
+package hello.core.autowired;
+
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class); // 스프링 컨테이너에 해당 설정파일이 스프링빈으로 등록
+
+    }
+
+    static class TestBean { // 테스트 하려는 클래스
+
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) { // @Nullable 패키지 잘 보고 가져오기 (springframework 꺼로)
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
